da4d425fa0063ad398ac46a01e28a573533ed211
xrkawa <76703454+xrkawa@users.noreply.github.com>
2021-02-05 16:04:41 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -16,22 +16,22 @@ module.exports = {
     .setTitle("Help")
   .setImage("https://cdn.discordapp.com/attachments/776114986879549471/807229581962510366/image0.gif")
   .setDescription([  SUPPORT  ](https://discord.gg/Z7JgEkGtak) - [   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=787948438700032030&permissions=1647705169&scope=bot) 
-
-prefix=\${prefix}`
+\`
+prefix=${prefix}
 
 
 
 Music
 
-`${prefix}filter(fi)` - `${prefix}loop(l)` 
-`${prefix}lyrics(ly)` - `${prefix}np`
-`${prefix}pause` - `${prefix}play(p)` 
-`${prefix}queue(qu)` - `${prefix}radio`
-`${prefix}remove` - `${prefix}resume(r)`
-`${prefix}search` - `${prefix}volume(vol)` 
-`${prefix}shuffle(mix)` - `${prefix}skip(s)` 
-`${prefix}skipto(st)` - `${prefix}stop`
-
+${prefix}filter(fi) - ${prefix}loop(l)
+${prefix}lyrics(ly) - ${prefix}np
+${prefix}pause - ${prefix}play(p)
+${prefix}queue(qu) - ${prefix}radio
+${prefix}remove - ${prefix}resume(r)
+${prefix}search - ${prefix}volume(vol)
+${prefix}shuffle(mix) - ${prefix}skip(s)
+${prefix}skipto(st) - ${prefix}stop
+\`
 
 
 `)
