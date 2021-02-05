2c97a8fed13c12641c2159a4a9ecd7324c460be4
xrkawa <76703454+xrkawa@users.noreply.github.com>
2021-02-05 15:46:34 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -1,129 +1,89 @@
-const { Client, Collection, MessageEmbed } = require(`discord.js`);
-const { 
-  PREFIX, 
-} = require(`../config.json`);
+const { MessageEmbed } = require("discord.js");
+
+  const prefix = ("p.");
+
 
 module.exports = {
-  name: `help`,
-  description: `Gives you a list of all help Commands`,
-  aliases: ["h","commands"],
-  cooldown: 3,
-  edesc: "Type help to get a short preview of all Commands, Type help <COMMANDNAME> to get extended information about this one command!",
-  execute(message,args,client) {
-     
+  name: "help",
+  aliases: ["h"],
+  description: "Display all commands and descriptions",
+  execute(message) {
     let commands = message.client.commands.array();
- 
+
     let helpEmbed = new MessageEmbed()
-      .setTitle("MBWF-v1 Help")
-      .setDescription(`**Version:** \`v2.8\` \n**PREFIX:** \`${PREFIX}\``)
-      .setFooter( client.user.username +`Type: ${PREFIX}help <Command>  for more information!`)
-      .setColor("#c219d8");
+    .setFooter(message.author.username, message.author.displayAvatarURL)
+    .setColor('RANDOM')
+    .setTitle("Help")
+  .setImage("https://cdn.discordapp.com/attachments/776114986879549471/807229581962510366/image0.gif")
+  .setDescription([  SUPPORT  ](https://discord.gg/Z7JgEkGtak) - [   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=787948438700032030&permissions=1647705169&scope=bot) 
 
-      let ifargstruedothis = -1;
-      
-      switch(args[0]){
-          case "filter":
-           ifargstruedothis=0;
-          break;
-          case "loop":
-            ifargstruedothis=1;
-          break;
-          case "lyrics":
-            ifargstruedothis=2
-          break;
-          case "nowplaying":
-            ifargstruedothis=3
-          break;
-          case "pause":
-            ifargstruedothis=4
-          break;
-          case "play":
-            ifargstruedothis=5
-          break;
-          case "playlist":
-            ifargstruedothis=6
-          break;
-          case "queue":
-            ifargstruedothis=7
-          break;
-          case "radio":
-            ifargstruedothis=8
-          break;
-          case "remove":
-            ifargstruedothis=9
-          break;
-          case "resume":
-            ifargstruedothis=10
-          break;
-          case "search":
-            ifargstruedothis=11
-          break;
-          case "shuffle":
-            ifargstruedothis=12
-          break;
-          case "skip":
-            ifargstruedothis=13
-          break;
-          case "skipto":
-            ifargstruedothis=14
-          break;
-          case "stop":
-            ifargstruedothis=15
-          break;
-          case "volume":
-            ifargstruedothis=16
-          break;
-          case "help":
-            ifargstruedothis=17
-          break;
-          default:        
-            commands.forEach((cmd) => {
-              helpEmbed.addField(
-                `**${message.client.prefix}${cmd.name}**`,
-                `${cmd.description}`,
-                true
-              );
-            });
-          if(!message.guild) {
-            if(!args[0]) {message.react("✅");return message.author.send(helpEmbed);}
-            return
-            }
-            message.react("✅");
-            message.author.send(new MessageEmbed().setColor("#c219d8")
-            .setDescription(`**👍 Sent from <#${message.channel.id}>**`))
-            message.author.send(helpEmbed)
-            message.channel.send( new MessageEmbed().setColor("#c219d8")
-            .setDescription(`**👍 ${message.author} Check your \`direct messages\` for a list of Commands!**`)
-            );
-           
-        break;
-       }
-     
-       if(ifargstruedothis>=0){
-         let aliases = commands[ifargstruedothis].aliases;
-         if(aliases === undefined || !aliases) aliases="No Aliases!";
-         let cooldown = commands[ifargstruedothis].cooldown;
-         if(cooldown === undefined || !cooldown) cooldown="No Cooldown!";
+prefix=\${prefix}`
 
 
-        helpEmbed.addField(
-          `**${message.client.prefix}${commands[ifargstruedothis].name}**`,
-          `\`\`\`fix\n${commands[ifargstruedothis].edesc}\n\`\`\`\n\`${commands[ifargstruedothis].description}\``
-        );
-        helpEmbed.addField(
-          `**:notes: Aliases:**`,
-          `\`${aliases}\``
-        );
-        helpEmbed.addField(
-          `**:wrench: Cooldown:**`,
-          `\`${cooldown}\``
-        );
-        if(!message.guild) return message.author.send(helpEmbed);
-          message.author.send(helpEmbed)
-          message.channel.send( new MessageEmbed().setColor("#c219d8")
-          .setDescription(`**👍 ${message.author} Check your \`direct messages\` for a list of Commands!**`)
-          );
-       }
 
-}
-} 
+Music
+
+`${prefix}filter(fi)` - `${prefix}loop(l)` 
+`${prefix}lyrics(ly)` - `${prefix}np`
+`${prefix}pause` - `${prefix}play(p)` 
+`${prefix}queue(qu)` - `${prefix}radio`
+`${prefix}remove` - `${prefix}resume(r)`
+`${prefix}search` - `${prefix}volume(vol)` 
+`${prefix}shuffle(mix)` - `${prefix}skip(s)` 
+`${prefix}skipto(st)` - `${prefix}stop`
+
+
+
+`)
+
+
+    helpEmbed.setTimestamp();
+
+    return message.channel.send(helpEmbed).catch(console.error);
+  }
+};
+Robot.WiLdERYesterday at 12:02 AM
+const { MessageEmbed } = require("discord.js");
+
+  const prefix = ("p.");
+
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  description: "Display all commands and descriptions",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+    .setFooter(message.author.username, message.author.displayAvatarURL)
+    .setColor('RANDOM')
+    .setTitle("Help")
+  .setImage("https://cdn.discordapp.com/attachments/776114986879549471/807229581962510366/image0.gif")
+  .setDescription(`[  SUPPORT  ](https://discord.gg/Z7JgEkGtak) - [   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=787948438700032030&permissions=1647705169&scope=bot) 
+\`
+prefix=${prefix}
+
+${prefix}help - ${prefix}uptime 
+${prefix}ping - ${prefix}prefix
+
+Music
+
+${prefix}filter(fi) - ${prefix}loop(l)
+${prefix}lyrics(ly) - ${prefix}np
+${prefix}pause - ${prefix}play(p) 
+${prefix}queue(qu) - ${prefix}radio
+${prefix}remove - ${prefix}resume(r)
+${prefix}search - ${prefix}volume(vol) 
+${prefix}shuffle(mix) - ${prefix}skip(s) 
+${prefix}skipto(st) - ${prefix}stop
+\`
+
+`)
+
+
+    helpEmbed.setTimestamp();
+
+    return message.channel.send(helpEmbed).catch(console.error);
+  }
+};
