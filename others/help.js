172de7c8fa48720db188b5610d130447ba12587b
xrkawa <76703454+xrkawa@users.noreply.github.com>
2021-02-05 16:11:19 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -1,86 +1,40 @@
 const { MessageEmbed } = require("discord.js");
 
   const prefix = ("p.");
-
-
-module.exports = {
-  name: "help",
-  aliases: ["h"],
-  description: "Display all commands and descriptions",
-  execute(message) {
-    let commands = message.client.commands.array();
-
-    let helpEmbed = new MessageEmbed()
-    .setFooter(message.author.username, message.author.displayAvatarURL)
-    .setColor('RANDOM')
-    .setTitle("Help")
-  .setImage("https://cdn.discordapp.com/attachments/776114986879549471/807229581962510366/image0.gif")
-  .setDescription(`[  SUPPORT  ](https://discord.gg/Z7JgEkGtak) - [   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=787948438700032030&permissions=1647705169&scope=bot) 
-\`
-prefix=${prefix}
-
-
-
-Music
-
-${prefix}filter(fi) - ${prefix}loop(l)
-${prefix}lyrics(ly) - ${prefix}np
-${prefix}pause - ${prefix}play(p)
-${prefix}queue(qu) - ${prefix}radio
-${prefix}remove - ${prefix}resume(r)
-${prefix}search - ${prefix}volume(vol)
-${prefix}shuffle(mix) - ${prefix}skip(s)
-${prefix}skipto(st) - ${prefix}stop
-\`
-
-
-`)
-
-
-    helpEmbed.setTimestamp();
-
-    return message.channel.send(helpEmbed).catch(console.error);
-  }
-};
-
-const { MessageEmbed } = require("discord.js");
-
-  const prefix = ("p.");
-
+  
 
 module.exports = {
   name: "help",
   aliases: ["h"],
   description: "Display all commands and descriptions",
+  cooldown: 3,
+  edesc: "Type help to get a short preview of all Commands, Type help <COMMANDNAME> to get extended information about this one command!",
   execute(message) {
     let commands = message.client.commands.array();
-
+     
     let helpEmbed = new MessageEmbed()
     .setFooter(message.author.username, message.author.displayAvatarURL)
     .setColor('RANDOM')
-    .setTitle("Help")
-  .setImage("https://cdn.discordapp.com/attachments/776114986879549471/807229581962510366/image0.gif")
-  .setDescription(`[  SUPPORT  ](https://discord.gg/Z7JgEkGtak) - [   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=787948438700032030&permissions=1647705169&scope=bot) 
-\`
-prefix=${prefix}
+    .setTitle("Help Command")
+    .setImage("https://cdn.discordapp.com/attachments/776114986879549471/807229581962510366/image0.gif")
+  .setDescription(`[  Support  ](https://discord.gg/Z7JgEkGtak) - [  Invite  ](https://discord.com/api/oauth2/authorize?client_id=787948438700032030&permissions=1647705169&scope=bot)
 
-${prefix}help - ${prefix}uptime 
-${prefix}ping - ${prefix}prefix
+\`prefix\` : \`p.\`
 
-Music
+🎶__Music Command__
+\`filter(fi)\`  -  \`loop(l)\`
+\`lyrics(ly)\`  -  \`np\`
+\`pause\`  -  \`play(p)\`
+\`queue(qu)\`  -  \`radio\`
+\`remove\`  -  \`resume(r)\`
+\`search\`  -  \`shuffle(mix)\`
+\`skip(s)\`  -  \`skipto(st)\`
+\`stop\`  -  \`volume(vol)\`
 
-${prefix}filter(fi) - ${prefix}loop(l)
-${prefix}lyrics(ly) - ${prefix}np
-${prefix}pause - ${prefix}play(p) 
-${prefix}queue(qu) - ${prefix}radio
-${prefix}remove - ${prefix}resume(r)
-${prefix}search - ${prefix}volume(vol) 
-${prefix}shuffle(mix) - ${prefix}skip(s) 
-${prefix}skipto(st) - ${prefix}stop
-\`
 
 `)
-
+    
+       
 
     helpEmbed.setTimestamp();
 
