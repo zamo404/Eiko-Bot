06cb2b9ae1539845ddfa00d97fc860e3b3f41411
BlackMan✓ <78313191+BlackMan69x@users.noreply.github.com>
2021-11-23 15:36:34 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -14,7 +14,7 @@ module.exports = {
      
     let helpEmbed = new MessageEmbed()
     .setFooter(message.author.username, message.author.displayAvatarURL)
-    .setColor('RANDOM')
+    .setColor('F2254C')
     .setTitle("Help Command")
     .setImage("https://cdn.discordapp.com/attachments/806536956737355788/810923065092472843/20-17-22-7d20.gif")
   .setDescription(`[  Support  ](https://discord.gg/rSXquTjmU3) - [  Invite  ](https://discord.com/api/oauth2/authorize?client_id=912442458409943091&permissions=534761045968&scope=bot)
