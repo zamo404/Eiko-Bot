db3333d9b0a4ee602f35200ac4872ab87f8456ae
BlackMan✓ <78313191+BlackMan69x@users.noreply.github.com>
2021-11-24 18:47:01 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -18,13 +18,6 @@ module.exports = {
     .setTitle("Help Command")
     .setImage("https://cdn.discordapp.com/attachments/806536956737355788/810923065092472843/20-17-22-7d20.gif")
   .setDescription(`[  Support  ](https://discord.gg/rSXquTjmU3) - [  Invite  ](https://discord.com/api/oauth2/authorize?client_id=912442458409943091&permissions=534761045968&scope=bot)
-**User Commands**
-\`invite(inv,addbot)\`  -  \`support(supp)\`
-\`about\`  -  \`ping\`
-\`uptime(upt)\`  -  \`vote\`
-\`prefix(px)\`
-
-
 **Music Command**
 \`filter(fi)\`  -  \`loop(l)\`
 \`lyrics(ly)\`  -  \`np\`
