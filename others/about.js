9244cb6dd777968625912f82da521a1ce63ce042
ANOxDevV <88611334+ANOxDevV@users.noreply.github.com>
2022-01-06 20:20:42 +0300

    Create about.js

---
+++ others/about.js
@@ -0,0 +1,42 @@
+const { MessageEmbed } = require("discord.js");
+
+  const prefix = ("-");
+  
+
+module.exports = {
+  name: "invite",
+  aliases: ["h"],
+  description: "Display all commands and descriptions",
+  cooldown: 3,
+  edesc: "Type help to get a short preview of all Commands, Type help <COMMANDNAME> to get extended information about this one command!",
+  execute(message) {
+    let commands = message.client.commands.array();
+     
+    let helpEmbed = new MessageEmbed()
+    .setFooter(message.author.username, message.author.displayAvatarURL)
+    .setColor('BLACK')
+    .setTitle("Roxy Invite")
+    .setImage("")
+  .setDescription(`
+⭐️ **Name Bot** : <@922752221232369694>
+
+••••••
+
+🛠 **Creators** : <@922530000618528778>
+
+••••••
+
+⚡️ **Languages** : **JavaScript**
+`)
+    
+ const disbut = require("discord-buttons")
+ let button = new disbut.MessageButton()
+        .setStyle('url')
+        .setLabel('Invite')
+        .setURL('https://discord.com/api/oauth2/authorize?client_id=922752221232369694&permissions=8&scope=bot');////سيرفرك
+
+    helpEmbed.setTimestamp();
+
+    return message.channel.send({ embed : helpEmbed , buttons : [button] }).catch(console.error);
+  }
+};
