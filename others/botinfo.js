4af4d16d782f20d7838321688ab85fa3ced28da8
ANOxDevV <88611334+ANOxDevV@users.noreply.github.com>
2022-01-23 14:10:20 +0300

    Update botinfo.js

--- others/botinfo.js
+++ others/botinfo.js
@@ -25,11 +25,11 @@ module.exports = {
 
 <:ano_4:923558596833341481> **__Admin Bot__** : <@807236065001865217>
 
-⭐️ **__Servers__** : \`${client.guilds.cache.size}\`
+⭐️ **__Servers__** : ${client.guilds.cache.size}
 
-🗂 **__Channels__** : \`${client.channels.cache.size}\`
+🗂 **__Channels__** : ${client.channels.cache.size}
 
-🏓 **__My Ping__** : \`${client.ws.ping}\`
+🏓 **__My Ping__** : ${client.ws.ping}
 
 `)
     
